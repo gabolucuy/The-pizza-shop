0b80bc4cfeab40b4ec1662f922e4858624b72298
gabolucuy <gabolucuy@hotmail.com>
2019-02-18 09:50:30 -0400

    Add seed file with some data to fill database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,42 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#Cheeses
+Cheese.create(name: "Cheddar")
+Cheese.create(name: "Bergenost")
+Cheese.create(name: "Colby")
+
+#Sauces
+Sauce.create(name: "BBQ")
+Sauce.create(name: "Hot")
+Sauce.create(name: "Honey Garlic")
+Sauce.create(name: "Honey Mustard")
+
+#Crust
+Crust.create(name: "Thin")
+Crust.create(name: "Thick")
+Crust.create(name: "Filled with cheese")
+
+#Sizes
+Size.create(name: "Personal", slices: 2)
+Size.create(name: "Small", slices: 4)
+Size.create(name: "Medium", slices: 6)
+Size.create(name: "Large" , slices: 8)
+
+#Crusts
+Crust.create(name: "Thin")
+Crust.create(name: "Thick")
+Crust.create(name: "Filled with cheese")
+
+#Ingredients
+(1..10).each do
+	Ingredient.create(name: Faker::Food.ingredient)
+end
+
+#Pizza types
+PizzaType.create(name: "Hawaiian")
+PizzaType.create(name: "Brazilian")
+PizzaType.create(name: "Veggie")
+PizzaType.create(name: "Simple")
+
+#Other dishes
+NonPizza.create(name: "Spaghetti")
+NonPizza.create(name: "Lasagna")
+NonPizza.create(name: "Salad")
